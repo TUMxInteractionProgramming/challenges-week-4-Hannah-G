aca3b4d2cc057d736bce5dbb87829e32c9ea0032
Hannah Gramm <hannah.gramm@tum.de>
2019-04-05 10:50:28 +0200

    upload

--- js/channels.js
+++ js/channels.js
@@ -1,45 +1,52 @@
-// #7 New global variable
+
 var yummy = {
-    name: "#Yummy",
-    createdOn: new Date(2016, 3, 1),
-    createdBy: 'minus.plus.yummy',
+    name: '#Yummy',
+    createdOn: new Date('April 1, 2016'),
+    CreatedBy: 'minus.plus.yummy',
     starred: false,
-    expiresIn: 100,
+    expiresIn: 100*60*1000,
     messageCount: 999,
-}
+    location: 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>'
+};
 
-var sevencontinents = {
+var sevenContinents = {
     name: '#SevenContinents',
-    createdOn: new Date(2016, 3, 1),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date('April 1, 2016'),
+    CreatedBy: 'minus.plus.yummy',
     starred: true,
-    expiresIn: 100,
+    expiresIn: 100*60*1000,
     messageCount: 999,
-}
+    location: 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>'
+};
 
-var killerapp = {
+var killerApp = {
     name: '#KillerApp',
-    createdOn: new Date(2016, 3, 1),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date('April 1, 2016'),
+    CreatedBy: 'minus.plus.yummy',
     starred: false,
-    expiresIn: 100,
+    expiresIn: 100*60*1000,
     messageCount: 999,
-}
+    location: 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>'
+};
 
-var firstpersononmars = {
+
+var firstPersonOnMars = {
     name: '#FirstPersonOnMars',
-    createdOn: new Date(2016, 3, 1),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date('April 1, 2016'),
+    CreatedBy: 'minus.plus.yummy',
     starred: true,
-    expiresIn: 100,
+    expiresIn: 100*60*1000,
     messageCount: 999,
-}
+    location: 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>'
+};
+
 
 var octoberfest = {
     name: '#Octoberfest',
-    createdOn: new Date(2016, 3, 1),
-    createdBy: 'minus.plus.yummy',
+    createdOn: new Date('April 1, 2016'),
+    CreatedBy: 'minus.plus.yummy',
     starred: false,
-    expiresIn: 100,
+    expiresIn: 100*60*1000,
     messageCount: 999,
-}
+    location: 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>'
+};
--- js/script.js
+++ js/script.js
@@ -1,75 +1,61 @@
 /* #6 start the #external #action and say hello */
 console.log("App is alive");
 
-/**
- * #6 #Switcher function for the #channels name in the right app bar
- * @param channelName Text which is set
- */
+// CHANNELS ––––––––––––––––––––––––––––––––––––––––––––––––––
 
-
-// #7 GLOBAL VARIABLES
-var currentChannel = {}
-
+// current channel location
+var currentChannel = {};
+// my own channel location
 var currentLocation = {
-    longitude: 48.112108,
-    latitude: 11.576861,
-    what3words: 'dichterin.übungen.ausfällt'
-}
+    longitude: 48.112076,
+    latitude: 11.576779,
+    what3words: '<a href="https://map.what3words.com/season.texted.trendy" target="_blank"><strong>season.texted.trendy</strong></a>'
+};
 
+console.log ("currentLocation", currentLocation);
+
+
+
+// SWITCH CHANNELS
 function switchChannel(channelName) {
+
+    //#7 store channelName in the global variable currentChannel
+    currentChannel = channelName;
+    //#7 Log the currentChannel
+    console.log("currentChannel", currentChannel);
+
     //Log the channel switch
     console.log("Tuning in to channel", channelName);
 
-    // #7 Write the new channel to the right app bar
-    $('#channel-name').html(channelName.name);
+    //Write the new channel to the right app bar
+    document.getElementById('channel-name').innerHTML = channelName.name;
 
     //#6 change the #channel #location
-    $("#channel-location").html('by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>');
+    document.getElementById('channel-location').innerHTML = channelName.location;
 
-    /* #7 star depending on object property */
-    if (channelName.starred == true){
-        // $('#channel-star').removeClass('far fa-star');
-        // $('#channel-star').addClass('fas fa-star');
+    /* #7 #liking channels on #click */
+    (channelName.starred == true) ?
+        $('#channel-star').attr('class', 'fas fa-star') :
+        $('#channel-star').attr('class', 'far fa-star')
 
-    } else{
-        // $('#channel-star').removeClass('fas fa-star');
-        // $('#channel-star').addClass('far fa-star');
-        $('#channel-star').toggleClass('fas fa-star far fa-star');
-    }
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
     $('#channels li:contains(' + channelName.name + ')').addClass('selected');
 
-    // #7 Setting channelName to currentChannel variable
-    currentChannel = channelName;
-
-    // Test console logs
-    // console.log("Current Channel", currentChannel);
-    // console.log("Current location", currentLocation);
 }
 
-/* #7 #liking a channel on #click */
+/* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').toggleClass('fas fa-star far fa-star');
-
-    // #7 writing new Status of Star in the refering object
-    currentChannel.starred = !currentChannel.starred
+    $('#channel-star').toggleClass('fas fa-star');
+    $('#channel-star').toggleClass('far fa-star');
 }
 
-// #7 function starList() {
-//     // jQuery to display correct star in channel list
-//     $('.fas').toggleClass('fas fa-star far fa-star');
 
-//     // writing Status of Star in the refering object
-//     currentChannel.starred = !currentChannel.starred
-// }
 
-/**
- * #6 #taptab selects the given tab
- * @param tabId #id of the tab
- */
+// TABBAR ––––––––––––––––––––––––––––––––––––––––––––––––––
+
 function selectTab(tabId) {
     // #6 #taptab #remove selection from all buttons...
     $('#tab-bar button').removeClass('selected');
@@ -77,17 +63,31 @@ function selectTab(tabId) {
     //...#6 #taptab #log the new tab on change...
     console.log('Changing to tab', tabId);
 
-    //...#6 #taptab #add selection to the given tab button, its id is passed via the #argument tabId
+    //...#6 #taptab #add selection to the given tab button
     $(tabId).addClass('selected');
 }
 
-/**
- * #6 #toggle (show/hide) the emojis menu #smile
- */
-function toggleEmojis() {
-    /* $('#emojis').show(); // #show */
-    $('#emojis').toggle(); // #toggle
-}
+// INPUT AREA ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+    function toggleEmojis(){
+            $('#emojis').toggle(); // #toggle
+        }
+
+
+
+//#8 current date plus 15 minutes
+var now15 = new Date();
+now15.setMinutes (now15.getMinutes() +15);
+now15 = new Date(now15);
+
+//#8 current date
+var now = Date.now();
+
+//#8 expiresIn
+var expiresInMilli = (now + 900000) - now;
+var expiresInMin = expiresInMilli / 60000;
+
+// MESSAGES––––––––––––––––––––––––––––––––––––––––––––––––––
 
 // #8 Constructor Function for new chat messages
 function Message(text){
@@ -97,38 +97,96 @@ function Message(text){
     this.createdOn = Date.now();
     this.expiresOn = this.createdOn + (1000 /*sec*/ * 60 /*min*/ * 15 /*hour*/ * 1 /*day*/ * 1);
     this.text = text;
-    own = true;
+    this.own = true;
+}
+
+// #8 sendMessage function
+function sendMessage() {
+    var writtenMessage = $('#input-message').val();
+
+    var newMessage = new Message (writtenMessage);
+
+    $('#messages').append(createMessageElement(newMessage));
+
+    // #8 input is cleared after sending a message
+    $('#input-message').val('');
 }
 
 // #8 sendMessage function
 function sendMessage(){
     // creating new Message Element
     var sentMessage = new Message($('#messageInput').val());
+    console.log(sentMessage);
+
+    // Clearing input field
+    $('#messageInput').val('');
 
     // Calling createMessageElement Function and appending it to Messages
     $('#messages').append(createMessageElement(sentMessage));
 
-    // Clearing Message field
-    $('#messageInput').val('');
-
-    // Logging of Sent Message
-    console.log('This created by ' + sentMessage.createdBy);
-    console.log('created on ' + sentMessage.createdOn);
-    // console.log('date now  ' + Date.now());
-    // Currently not working
-    console.log('expires on ' + sentMessage.expiresOn);
-    console.log('This is the sent Message ' + sentMessage.text);
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
 }
 
 // #8 create message Element function
 function createMessageElement(messageObject){
-    
-    // new epxires in variable
-    console.log('expires on ' + messageObject.expiresOn);
-    var expiresIn = Math.round(Date.now() - messageObject.expiresOn);
-    
-    console.log('expires in ' + expiresIn);
 
-    // creating new html elements
-    return $('<div>').addClass('message').append('<h3><a href="'+messageObject.createdBy+'" target="_blank"><strong>'+messageObject.createdBy+'</strong></a> '+messageObject.createdOn+' <em>:expiresIn: min. left</em></h3> <p>'+messageObject.text+'</p><button>+5 min.</button>');
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    const dayNames = [
+        "Mon", "Tue", "Wed",
+        "Thu", "Fri", "Sat", "Sun"
+    ];
+
+    // new expires in variable
+    var expiresIn = Math.round((messageObject.expiresOn - messageObject.createdOn) / (60*1000) )
+
+    // Temp parts of date to combine to string in datetostring
+    var dTemp = new Date (messageObject.createdOn);
+    var weekdayTemp = dTemp.getDay()-1;
+    var dateTemp = dTemp.getDate();
+    var monthTemp = dTemp.getMonth();
+    var hourTemp = dTemp.getHours();
+    var minutesTemp = dTemp.getMinutes();
+
+    //  Combining parts of date top string by looking up values in const array at begging of function
+
+    var dateToString =
+        dayNames[weekdayTemp]
+        + ', '+
+        monthNames[monthTemp]
+        +' '+
+        dateTemp
+        +'th, '+
+        hourTemp
+        +':'+
+        minutesTemp;
+
+    // retruning element that creates div with jQuery and appends all other channel meta
+    return $('<div>').addClass('message own').append('<h3><a href="http://w3w.co/'+messageObject.createdBy+'" target="_blank"><strong>'+messageObject.createdBy+'</strong></a> '+dateToString+' <em>'+expiresIn+' min. left</em></h3> <p>'+messageObject.text+'</p><button>+5 min.</button>');
+}
+
+
+// CHANNELS LIST ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+// #8 function to list
+function listChannels() {
+    $('ul').append(createChannelElement(yummy));
+    $('ul').append(createChannelElement(sevenContinents));
+    $('ul').append(createChannelElement(killerApp));
+    $('ul').append(createChannelElement(firstPersonOnMars));
+    $('ul').append(createChannelElement(octoberfest));
+}
+
+function createChannelElement(channelName) {
+    var channelListItem = $('<li onclick="switchChannel();">').text(channelName.name);
+    var channelClassMeta = $('<span>').addClass('channel-meta').appendTo(channelListItem);
+    $('<i>').addClass(channelName.starred ? 'fas fa-star':'far fa-star').appendTo(channelClassMeta);
+    $('<span>').text((channelName.expiresIn / 60000) +' min').appendTo(channelClassMeta);
+    $('<span>').text(channelName.messageCount+ ' new').appendTo(channelClassMeta);
+    $('<i>').addClass('fa fa-chevron-right').appendTo(channelClassMeta);
+
+
+    return channelListItem;
 }
